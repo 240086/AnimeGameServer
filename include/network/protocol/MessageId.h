69f846f1218ece9ecf33affccd1499dd21ff43f3
240086 <2686251536@qq.com>
2026-03-13 16:07:16 +0800

    feat(protocol): extend MessageId and define protocol v1

--- include/network/protocol/MessageId.h
+++ include/network/protocol/MessageId.h
@@ -2,9 +2,20 @@
 
 enum MessageId
 {
+    // client -> server
+
     MSG_LOGIN = 1,
     MSG_GACHA = 2,
     MSG_HEARTBEAT = 3,
+    MSG_GACHA_TEN = 4,
+    MSG_GACHA_HISTORY = 5,
+
+    // server -> client
+
+    MSG_LOGIN_RESPONSE = 1000,
+
     MSG_GACHA_DRAW = 1001,
-    MSG_GACHA_DRAW_TEN = 1002
+    MSG_GACHA_DRAW_TEN = 1002,
+
+    MSG_GACHA_HISTORY_RESPONSE = 1003
 };
